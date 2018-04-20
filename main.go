a35ebac524ea2f9769a496ffa5992fe98f834bd9
Deliana Escobari <descobari@enova.com>
2018-04-19 22:07:09 -0500

    Test init port in main

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/jtacoma/uritemplates"
 )
 
 var (
-	port    string
+	// port    string
 	baseURL string
 )
 
@@ -21,12 +25,13 @@ func init() {
 	}
 
 	// Load environment variables
-	port = os.Getenv("PORT")
+	// port = os.Getenv("PORT")
 	baseURL = os.Getenv("XKCD_BASE_URL")
 }
 
 func main() {
 	// Setup your handlers!
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", index)
 	http.HandleFunc("/rendercomic", renderComic)
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
@@ -34,4 +39,74 @@ func main() {
 	// Start up your server!
 	fmt.Printf("Starting program.\nListening on port %s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
+
+func renderComic(w http.ResponseWriter, r *http.Request) {
+	xkcd, err := fetchComic()
+	if err != nil {
+		xkcd = &Comic{
+			Title:  "Not a real title",
+			Year:   "1969",
+			ImgURL: "not a real url",
+		}
+	}
+	fmt.Println(xkcd.Title)
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, xkcd)
+}
+
+const latestComicNum = 1000
+
+type Comic struct {
+	Title  string `json:"title"`
+	Year   string `json:"year"`
+	ImgURL string `json:"img"`
+}
+
+func fetchComic() (*Comic, error) {
+	comicNum := rand.Intn(latestComicNum)
+	fmt.Println("Generated random comic number to retrieve:", comicNum)
+	template, err := uritemplates.Parse(baseURL)
+	if err != nil {
+		return nil, err
+	}
+
+	url, err := template.Expand(map[string]interface{}{"comicNo": comicNum})
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("get request url", url)
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error status response: %d", response.StatusCode)
+	}
+
+	// this can error due to reasons that are hard
+	// to produce in tests such as network resets
+	// on the given socket
+	// respBody, err := ioutil.ReadAll(response.Body)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	fmt.Println("response body", response.Body)
+	c := &Comic{}
+	json.NewDecoder(response.Body).Decode(c)
+
+	fmt.Println("comic title:", c.Title)
+
+	return c, nil
+
 }
