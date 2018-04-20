a93f546892466a1465f7608fd36b3f8ee9099df9
Deliana Escobari <descobari@enova.com>
2018-04-19 21:25:35 -0500

    Add src folder to support gb

---
+++ src/handlers.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
+
+func renderComic(w http.ResponseWriter, r *http.Request) {
+	xkcd, err := fetchComic()
+	if err != nil {
+		xkcd = &Comic{
+			Title:  "Not a real title",
+			Year:   "1969",
+			ImgURL: "not a real url",
+		}
+	}
+	fmt.Println(xkcd.Title)
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, xkcd)
+}
